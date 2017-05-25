c1c3f75f0d1b0cc43de9336d81590fc2906755d6
Juan Albahaca <juanalbahaca92@gmail.com>
2017-05-26 00:17:46 +0200

    Custom modelling for sign in ui

---
+++ SocialNetwork/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialNetwork
+//
+//  Created by Juan Carlos Albahaca on 2017-05-25.
+//  Copyright © 2017 Juan Carlos Albahaca. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120 / 255.0
